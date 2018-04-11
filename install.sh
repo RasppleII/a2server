aaea9245de2548199694db724f65c2645f529635
T. Joseph Carter <tjcarter@spiritsubstance.com>
2018-04-11 06:59:48 -0700

    All scripts now run from source dir
    Legacy code in the remaining scripts to have them run from the website
    using a2sDevel detection has been purged.  That doesn't mean everything
    now runs locally always—that's going to require a finer pick through the
    code I haven't done yet.  This is a start though, and doing it found a
    couple of bugs I've managed to fix already.

--- install.sh
+++ install.sh
@@ -11,17 +11,17 @@
 a2serverVersion="1.9.0"
 a2sScriptURL="https://raw.githubusercontent.com/RasppleII/a2server/master"
 
-a2sDevel="$( dirname "${BASH_SOURCE[0]}" )"
-if [[ -f "$a2sDevel/.a2server_source" ]]; then
-	pushd $a2sDevel >/dev/null
-	a2sDevel="$PWD"
-	popd >/dev/null
-else
-	printf "a2server: Cannot find a2server source tree relative to ${BASH_SOURCE[0]}\n\n"
+# Find the path of our source directory
+a2sSource="$( dirname "${BASH_SOURCE[0]}" )/.."
+pushd $a2sSource >/dev/null
+a2sSource="$PWD"
+popd >/dev/null
+if [[ ! -f "$a2sSource/.a2server_source" ]]; then
+	printf "\na2server: cannot find a2server source directory in $a2sSource.\n\n"
 	exit 1
 fi
 
 # Run the legacy setup script for anything not yet ported
-if [[ -e "${a2sDevel}/setup/ivan.sh" ]]; then
-	"${a2sDevel}/setup/ivan.sh" "$@"
+if [[ -e "${a2sSource}/setup/ivan.sh" ]]; then
+	"${a2sSource}/setup/ivan.sh" "$@"
 fi
